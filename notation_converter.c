15778894da0afe618d87e20987b4d4c9e5b54e25
markandrew0501 <149173570+markandrew0501@users.noreply.github.com>
2025-04-28 14:40:17 +0800

    Create notation_converter.c

---
+++ notation_converter.c
@@ -0,0 +1,10 @@
+/* Notation Converter CLI:
+ *
+ * Converts math expressions (infix, prefix, postfix) using
+ * expression trees.  Supports +, -, *, /, 0-9 operands.
+ *
+ * Discrete Structures 2 - Bicol University College of Science
+ * Bongon, Mark Andrew
+ * Callos, Maria Dorothy
+ * Pielago, Salve Adelle
+ */
